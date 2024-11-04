0a7c38c10809053c8ddf07e91a600308d172965a
deaa aldeen <deaaaldeen45112@gmail.com>
2024-11-04 12:31:04 +0300

    push

--- editor/scripts/run_cpp_docker.sh
+++ editor/scripts/run_cpp_docker.sh
@@ -2,6 +2,6 @@
 
 TARGET_PATH=$1
 docker run --rm -i \
-    -v backend_code-editor-storage:/app/code-editor-storage:ro \
+    -v collaborative-code-editor_code-editor-storage:/app/code-editor-storage:ro \
     -w "$TARGET_PATH" \
     gcc:latest sh -c "find . -name '*.cpp' | xargs g++ -o /tmp/myapp && /tmp/myapp"
